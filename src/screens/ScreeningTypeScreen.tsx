cc1a554c9f4035bcadc1bfda3207ddf37b878527
alyse kwok <alysekwok@lawn-128-61-8-30.lawn.gatech.edu>
2023-03-04 17:40:01 -0500

    add navigation for screening type screen

--- src/screens/ScreeningTypeScreen.tsx
+++ src/screens/ScreeningTypeScreen.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Layout } from "../components/Layout";
-import { VStack, Heading, Button } from "native-base";
+import { VStack, Heading, Button, Box, Select, CheckIcon, Center } from "native-base";
+import { MainRouterParams } from "../routers/MainRouter";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 function CreateButtonGroup() {
   return (
@@ -13,15 +16,35 @@ function CreateButtonGroup() {
   );
 }
 
-export const ScreeningTypeScreen = () => {
-  /***************		JSX		***************/
+function CreateSelect() {
+  const [service, setService] = React.useState("");
+  return <Center>
+      <Box maxW="300">
+        <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Screening Type" placeholder="Choose Service" _selectedItem={{
+        bg: "teal.600",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} onValueChange={itemValue => setService(itemValue)}>
+          <Select.Item label="Bipolar" value="bipolar" />
+          <Select.Item label="Anxiety" value="anxiety" />
+          <Select.Item label="Postnatal Depression" value="depression" />
+          <Select.Item label="Birth Trauma" value="trauma" />
+        </Select>
+      </Box>
+    </Center>;
+}
 
+export const ScreeningTypeScreen = () => {
+  const { navigate } =
+    useNavigation<NativeStackNavigationProp<MainRouterParams>>();
+
+  /***************		JSX		***************/
   return (
     <Layout>
       <VStack space={8} alignItems="center">
         <Heading textAlign="center">What are you screening for?</Heading>
-        <CreateButtonGroup />
-        <Button>Next</Button>
+        {/* <CreateButtonGroup /> */}
+        <CreateSelect/>
+        <Button onPress={() => navigate("Screening")}>Next</Button>
       </VStack>
     </Layout>
   );
--- src/screens/TutorialScreen.tsx
+++ src/screens/TutorialScreen.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Layout } from "../components/Layout";
 import {
-  Button,
   Text,
-  HStack,
   VStack,
   Heading,
   Box,
