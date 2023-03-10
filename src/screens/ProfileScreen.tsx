961cef45f5e21953a126e783baf30adcf28536b8
EvaTomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2023-03-10 18:00:41 -0500

    added UI for profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,8 +1,46 @@
-import { Text } from "native-base";
-import React from "react";
+import {
+  Card,
+  Heading,
+  VStack,
+  FormControl,
+  View,
+  Image,
+  Text
+} from "native-base";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { Layout } from "../components/Layout";
 
 export const ProfileScreen = () => {
+  const image = require("../images/p4m-profile.png");
+
   /***************		JSX		***************/
 
-  return <Text>Profile</Text>;
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Layout>
+        <FormControl isRequired>
+          <VStack space={5} paddingTop={10}>
+            <Heading textAlign="center">Profile</Heading>
+            <Card style={{ borderRadius: 8, backgroundColor: "#FBF4BB" }}>
+              <Text>Currently signed in as</Text>
+            </Card>
+            <Card style={{ borderRadius: 8, backgroundColor: "#D9D9D9" }}>
+              <Text>View saved diagnoses</Text>
+            </Card>
+            <VStack space={2}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flex: 1 }}></View>
+              </View>
+            </VStack>
+          </VStack>
+        </FormControl>
+        <Image
+          alignSelf="center"
+          source={image}
+          resizeMode="contain"
+          alt="P4M profile"
+        />
+      </Layout>
+    </TouchableWithoutFeedback>
+  );
 };
