c7f87d5591297485144dc200a6aa2bbd8d0ccf49
shwang315 <shwang315@gatech.edu>
2023-03-13 12:30:18 -0400

    adding perinatal calc logic

--- src/calc/perinatalAnxietyCalc.ts
+++ src/calc/perinatalAnxietyCalc.ts
@@ -1,3 +1,10 @@
 export const perinatalAnxietyCalc = (answers: number[]) => {
-
+  let sum = 0;
+  for (let i = 0; i < 31; i++) {
+    sum += answers[i];
+  }
+  if (sum >= 26) {
+    return true;
+  }
+  return false;
 };
