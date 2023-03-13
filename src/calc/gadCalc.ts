c26326d4560e59206986070fc0dde0c0a22af3c8
athenawu68 <athena.wu.vabzie@statefarm.com>
2023-03-12 23:28:59 -0400

    add algorithm for gad depression and console displays corresponding logs

--- src/calc/gadCalc.ts
+++ src/calc/gadCalc.ts
@@ -1,3 +1,19 @@
+var minimalAnxiety = false;
+var mildAnxiety = false;
+var moderateAnxiety = false;
+var severeAnxiety = false;
 export const gadCalc = (answers: number[]) => {
-
-};
+    let sum = 0;
+        for (let i = 0; i <= 12; i++) {
+            sum += answers[i];
+        }
+        if (sum >= 0 && sum <=4) {
+            minimalAnxiety = true;
+        } else if (sum >= 5 && sum <=9) {
+            mildAnxiety = true;
+        } else if (sum >= 10 && sum <=14){
+            moderateAnxiety = true;
+        } else if (sum >= 15 && sum <=21){
+            severeAnxiety = true;
+        }
+    };
