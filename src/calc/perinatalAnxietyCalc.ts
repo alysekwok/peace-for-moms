f3790f92efa6e72a2b73e19e11709accd7a33286
Fitton, Andrew David <afitton3@gatech.edu>
2023-03-13 12:18:29 -0400

    update algos

--- src/calc/perinatalAnxietyCalc.ts
+++ src/calc/perinatalAnxietyCalc.ts
@@ -1,3 +1,13 @@
 export const perinatalAnxietyCalc = (answers: number[]) => {
-
+    let sum = 0;
+    for (let i = 0; i < answers.length; i++) {
+        sum += answers[i];
+    }
+    if (sum <= 20) {
+        return "Asymptomatic";
+    } else if (sum > 20 && sum < 42) {
+        return "Mild-moderate symptoms";
+    } else {
+        return "Severe Symptoms";
+    }
 };
