54f27d6fae86227b69cf15f0792019e2699e58eb
alyse kwok <alysekwok@lawn-128-61-2-112.lawn.gatech.edu>
2023-04-02 21:27:42 -0400

    add styling for results page

--- src/screens/ResultsScreen.tsx
+++ src/screens/ResultsScreen.tsx
@@ -3,12 +3,9 @@ import { Layout } from "../components/Layout";
 import {
   VStack,
   Heading,
-  Button,
-  Box,
-  Select,
-  CheckIcon,
-  Center,
-  Text
+  Image,
+  Text, 
+  Box
 } from "native-base";
 import { MainRouterParams } from "../routers/MainRouter";
 import { useNavigation } from "@react-navigation/native";
@@ -17,6 +14,7 @@ import { useRoute } from "@react-navigation/native";
 
 
 export const ResultsScreen = () => {
+const image = require("../images/peace4moms-1.png");
   /***************		HOOKS		***************/
 
   const { navigate } =
@@ -30,10 +28,31 @@ export const ResultsScreen = () => {
   return (
     <Layout>
       <VStack space={8} alignItems="center" justifyContent="center">
-        <Heading textAlign="center">{route.params.result[0]}</Heading>
+        <Heading padding={2} textAlign="center">{route.params.result[0]}</Heading>
+
         <Text>Your score: {route.params.result[1]}</Text>
-        <Text>{route.params.result[2]}</Text>
+        <Box
+         p="3.5"
+         bg="yellow.100"
+         alignItems="center"
+         borderRadius={30}
+         justifyContent="center"
+         >
+            <Text textAlign="center" >{route.params.result[2]}</Text>
+        </Box>
+        <Box padding={5} alignItems="center">
+            <Text>Disclaimer: The PEACE for Moms mobile application does NOT save your screening results. Please screenshot this screen for your records if you would like to keep the results.</Text>
+        </Box>
+        {/* <Image
+          alignSelf="center"
+          source={image}
+          resizeMode="contain"
+          alt="P4M image"
+          width={200}
+          height={500}
+        /> */}
       </VStack>
+  
     </Layout>
   );
 };
